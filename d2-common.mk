fb8daa4ea9fd64f057ef8ffa858ebf95e69f3320
fezzy <fezzy102@gmail.com>
2013-02-18 17:13:47 -0500

    'update to the jelltbean2 branch'

--- d2-common.mk
+++ d2-common.mk
@@ -16,9 +16,6 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-## The gps config appropriate for this device
-PRODUCT_COPY_FILES += device/common/gps/gps.conf_US_SUPL:system/etc/gps.conf
-
 ## (2) Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/d2-common/d2-common-vendor.mk)
 
@@ -29,46 +26,36 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/d2-common/overlay
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
 # Audio configuration
 PRODUCT_COPY_FILES += \
-	device/samsung/d2-common/snd_soc_msm/snd_soc_msm:system/etc/snd_soc_msm/snd_soc_msm \
-	device/samsung/d2-common/snd_soc_msm/Call_DL_Rec:system/etc/snd_soc_msm/Call_DL_Rec \
-	device/samsung/d2-common/snd_soc_msm/Call_UL_DL_Rec:system/etc/snd_soc_msm/Call_UL_DL_Rec \
-	device/samsung/d2-common/snd_soc_msm/DSP_Loopback:system/etc/snd_soc_msm/DSP_Loopback \
-	device/samsung/d2-common/snd_soc_msm/HiFi:system/etc/snd_soc_msm/HiFi \
-	device/samsung/d2-common/snd_soc_msm/HiFi_Low_Power:system/etc/snd_soc_msm/HiFi_Low_Power \
-	device/samsung/d2-common/snd_soc_msm/HiFi_Rec:system/etc/snd_soc_msm/HiFi_Rec \
-	device/samsung/d2-common/snd_soc_msm/HiFi_Voice_Recognition:system/etc/snd_soc_msm/HiFi_Voice_Recognition \
-	device/samsung/d2-common/snd_soc_msm/Loopback:system/etc/snd_soc_msm/Loopback \
-	device/samsung/d2-common/snd_soc_msm/Video_Call:system/etc/snd_soc_msm/Video_Call \
-	device/samsung/d2-common/snd_soc_msm/Voice_Call1:system/etc/snd_soc_msm/Voice_Call1 \
-	device/samsung/d2-common/snd_soc_msm/VoIP1:system/etc/snd_soc_msm/VoIP1 \
-	device/samsung/d2-common/snd_soc_msm/VoIP2:system/etc/snd_soc_msm/VoIP2 \
-	device/samsung/d2-common/snd_soc_msm/VoIP3:system/etc/snd_soc_msm/VoIP3 \
-	device/samsung/d2-common/snd_soc_msm/VoIP4:system/etc/snd_soc_msm/VoIP4 \
-    device/samsung/d2-common/snd_soc_msm/VoIP5:system/etc/snd_soc_msm/VoIP5
-
-# modified ics hostapd for jb hotspot wpa support
-PRODUCT_COPY_FILES += \
-    device/samsung/d2-common/prebuilt/hostapd:system/bin/hostapd
+        device/samsung/d2-common/audio/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x \
+        device/samsung/d2-common/audio/audio_policy.conf:system/etc/audio_policy.conf \
+        device/samsung/d2-common/audio/audio_effects.conf:system/etc/audio_effects.conf
 
 # Ramdisk
-PRODUCT_COPY_FILES += \
-    device/samsung/d2-common/ramdisk/init.qcom.rc:root/init.qcom.rc \
-    device/samsung/d2-common/ramdisk/init.qcom.usb.rc:root/init.qcom.usb.rc \
-    device/samsung/d2-common/ramdisk/init.qcom.sh:root/init.qcom.sh \
-    device/samsung/d2-common/ramdisk/init.qcom.usb.sh:root/init.qcom.usb.sh \
-    device/samsung/d2-common/ramdisk/init.target.rc:root/init.target.rc \
-    device/samsung/d2-common/fstab.d2:root/fstab.d2 \
-    device/samsung/d2-common/ramdisk/initlogo.rle:root/initlogo.rle \
-    device/samsung/d2-common/ramdisk/ueventd.rc:root/ueventd.rc \
-	device/samsung/d2-common/ramdisk/init.emmc.rc:root/init.emmc.rc \
-	device/samsung/d2-common/ramdisk/init.bt.rc:root/init.bt.rc \
-	device/samsung/d2-common/ramdisk/init.sensor.rc:root/init.sensor.rc
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    initlogo.rle \
+    init.bt.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.target.rc \
+    ueventd.qcom.rc
+
+# Torch
+PRODUCT_PACKAGES += Torch
 
 # Vold configuration
 PRODUCT_COPY_FILES += \
     device/samsung/d2-common/vold.fstab:system/etc/vold.fstab
+
+# Wifi
+PRODUCT_PACKAGES += \
+    libnetcmdiface
 
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
@@ -77,8 +64,49 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # Lights
 PRODUCT_PACKAGES += lights.msm8960
 
+# Increase the HWUI font cache since we have tons of RAM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.text_cache_width=2048
+
+# QC Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=/system/lib/libqc-opt.so
+
+# QRNGD
+PRODUCT_PACKAGES += qrngd
+
+#common build.props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.hsxpa=1 \
+    ro.ril.gprsclass=10 \
+    persist.radio.add_power_save=1 \
+    persist.radio.snapshot_disabled=1 \
+    com.qc.hardware=true \
+    persist.radio.apm_sim_not_pwdn=1 \
+    ro.telephony.call_ring.multiple=0 \
+    ro.sf.lcd_density=320 \
+    ro.ril.transmitpower=true \
+    ro.opengles.version=131072 \
+    persist.audio.fluence.mode=endfire \
+    persist.audio.vr.enable=false \
+    persist.audio.handset.mic=digital \
+    persist.audio.speaker.location=high \
+    ro.qc.sdk.audio.fluencetype=fluence \
+    persist.timed.enable=true \
+    ro.emmc.sdcard.partition=17 \
+    ro.use_data_netmgrd=true \
+    persist.data_netmgrd_nint=16 \
+    lpa.decode=true \
+    rild.libpath=/system/lib/libril-qc-qmi-1.so \
+    ril.subscription.types=NV,RUIM \
+    ro.config.svlte1x=true \
+    ro.cdma.subscribe_on_ruim_ready=true \
+    persist.radio.no_wait_for_card=1 \
+    keyguard.no_require_sim=true \
+    media.aac_51_output_enabled=true
+
 # common msm8960
 $(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
 
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
